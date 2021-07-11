818f55e60c15ede234a5ad3e848505b3ff50f1fa
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-07-11 15:13:35 -0400

    Game Js ready

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,6 @@ const tie = 0;
 const win = 1;
 const lost = 2;
 
-let optionMachine = Math.floor(Math.random() * 3);
 
 let userOption = null;
 let rondas = 0
@@ -22,6 +21,7 @@ const rounds = () => {
 }
 
 function game (userOption) {
+let optionMachine = Math.floor(Math.random() * 3);
   if ( rondas > 0 ) {
     calcResult(userOption, optionMachine)
     rondas--
@@ -31,31 +31,38 @@ function game (userOption) {
 const calcResult = (userOption, optionMachine) => {
   if( userOption === optionMachine) { // Usuario y maquina escogen lo mismo
       resultadoFinal.innerHTML = `Empate!`;
+    console.log('Empate!');
 
   } else if ( userOption === rock ) { //Usuario escoge roca
 
     if (optionMachine === scissors) {
       resultadoFinal.innerHTML = `Que mal! Perdiste!`;
+    console.log('Una lastima! Perdiste!');
     }
     if ( optionMachine === paper ) {
       resultadoFinal.innerHTML = `Buena suerte! Ganaste!`;
+    console.log('Genial! Ganaste!');
     }
 
   } else if ( userOption === paper ) { // Usuario escoge papel
 
     if ( optionMachine === rock ) {
       resultadoFinal.innerHTML = `Que mal! Perdiste!`;
+    console.log('Una lastima! Perdiste!');
     }
     if ( optionMachine === scissors ) {
       resultadoFinal.innerHTML = `Buena suerte! Ganaste!`;
+    console.log('Genial! Ganaste!');
     }
   } else if ( userOption === scissors ) { // Usuario escoge tijeras
 
     if ( optionMachine === rock ) { 
       resultadoFinal.innerHTML = `Buena suerte! Ganaste!`;
+    console.log('Genial! Ganaste!');
     }
     if ( optionMachine === paper ) {
       resultadoFinal.innerHTML = `Que mal! Perdiste!`;
+    console.log('Una lastima! Perdiste!');
     }
   }
 
